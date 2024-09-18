2daa51114ee16b46e35c56ed453e7b27647669f0
Arisson Lima <arissonlima@gmail.com>
2024-09-18 16:06:39 -0300

    Initial commit

---
+++ components/Header/index.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./header.module.scss";
+
+import Logo from "/public/images/logo.svg";
+const Header = () => {
+  return(
+    <div className={styles.container}>
+      <div className={styles.logotipo}>
+        <Image src={Logo} />
+      </div>
+      <div className={styles.menu}>
+        <ul>
+          <Link href="/">Home</Link>
+          <Link href="/">O que fazemos</Link>
+          <Link href="/">Cases</Link>
+        </ul>
+      </div>
+      <div className={styles.action}>
+        <button className={styles.button}>Fale conosco</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import Header from "@/components/Header";
+
+import localFont from "next/font/local";
+import styles from "./Home.module.scss";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+}
